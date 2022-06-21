559d05c217a7f61aa1f1f07a1471bafacd9c3689
Filatov Denis <demalaron@yandex.ru>
2022-06-21 15:20:04 +0300

    add routes & controllers

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRouter = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.use(express.json());
+
+app.use('/', userRouter);
+
+// app.get('/users', (req, res) => {
+//   User.find({})
+//     .then((users) => res.status(200).send(users))
+//     .catch((err) => {
+//       res.status(500).send({ message: 'Произошла ошибка' });
+//       console.log(err);
+//       // console.log(err.name);
+//       // console.log(err.message);
+//       // console.log(err.errors);
+//     });
+// });
+
+// app.get('/users/:id', (req, res) => {
+//   User.findById(req.params.id)
+//     .then((user) => res.status(200).send(user))
+//     .catch((err) => {
+//       res.status(500).send({ message: 'Произошла ошибка' });
+//       console.log(err);
+//       // console.log(err.name);
+//       // console.log(err.message);
+//       // console.log(err.errors);
+//     });
+// });
+
+// app.post('/users', (req, res) => {
+//   const { name, about, avatar } = req.body;
+
+//   User.create({ name, about, avatar })
+//     .then((user) => res.send({ data: user }))
+//     .catch((err) => {
+//       res.status(500).send({ message: 'Произошла ошибка' });
+//       console.log(err);
+//       // console.log(err.name);
+//       // console.log(err.message);
+//       // console.log(err.errors);
+//     });
+// });
+
+app.listen(PORT);
---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+const User = require('../models/user');
+
+const getUsers = (_, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => {
+      res.status(500).send({ message: 'Произошла ошибка' });
+      console.log(err);
+      // console.log(err.name);
+      // console.log(err.message);
+      // console.log(err.errors);
+    });
+};
+
+const getUser = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      res.status(500).send({ message: 'Произошла ошибка' });
+      console.log(err);
+      // console.log(err.name);
+      // console.log(err.message);
+      // console.log(err.errors);
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      res.status(500).send({ message: 'Произошла ошибка' });
+      console.log(err);
+      // console.log(err.name);
+      // console.log(err.message);
+      // console.log(err.errors);
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+};
